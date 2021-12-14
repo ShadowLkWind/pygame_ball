51f143e8c218910aefcdef8ea9cd77d3b0bde273
666 <2395273102@qq.com>
2021-12-14 16:07:37 +0800

    none

--- pygame_ball/pygame_demo.py
+++ pygame_ball/pygame_demo.py
@@ -4,19 +4,24 @@ import pygame
 pygame.init()
 w = 900  # 宽
 h = 600  # 高
+num = 0  # 分数
 # 创建游戏窗口
 screen = pygame.display.set_mode((w, h), pygame.RESIZABLE)  # 保存窗口对象 RESIZABLE 可修改窗口大小
 image_maple = pygame.image.load("./image/maple.ico")  # 加载图片
 image_maple_rect = image_maple.get_rect()  # 获取矩形框
-image_xian = pygame.image.load("./image/666.png")
-image_xian_rect = image_xian.get_rect()
-image_xian_rect.x = 250
-image_xian_rect.y = 300
+image_xian = pygame.image.load("./image/666.png")  # 加载图片
+image_xian_rect = image_xian.get_rect()  # 获取矩形框
+image_xian_rect.x = 250  # 移动矩形框
+image_xian_rect.y = h - 200
 background1 = pygame.image.load("./image/1.png")  # 获取背景图1
 background2 = pygame.image.load("./image/2.png")  # 获取背景图2
 pygame.mixer.music.load("./music/1.mp3")  # 获取背景音乐
-pygame.mixer.music.play(-1, 0)  # 循环播放，从零秒开始
+pygame.mixer.music.set_volume(0.6)  # 缩小声音到60%
+pygame.mixer.music.play(-1)  # 循环播放
 clock = pygame.time.Clock()  # 时间模块
+text = pygame.font.SysFont('华文彩云', 22)  # 设置字体对象
+pengzhuang = pygame.mixer.Sound("./music/2.mp3")  # 加载音效
+pengzhuang.set_volume(0.9)  # 缩小声音到90%
 x = 1  # x轴的速度
 y = 1  # y轴的速度
 
@@ -78,7 +83,7 @@ while key:
             if event.buttons[0] == 1:
                 image_maple_rect = image_maple_rect.move(event.pos[0] - image_maple_rect.left - 64,
                                                          event.pos[1] - image_maple_rect.top - 64)
-
+    text_num = text.render("当前分数为：%s" % num, True, (0, 0, 0))  # 分数对象
     # 游戏内部逻辑处理
     # 绘制背景
     screen.blit(background1, (0, 0))
@@ -92,6 +97,7 @@ while key:
     # 绘制图片
     screen.blit(image_maple, image_maple_rect)
     screen.blit(image_xian, image_xian_rect)
+    screen.blit(text_num, (10, 10))
 
     # 撞墙回弹
     if image_maple_rect.top <= 0:
@@ -100,18 +106,21 @@ while key:
     if image_maple_rect.bottom >= h:
         if y > 0:
             y = -y
+            num -= 1
     if image_maple_rect.left <= 0:
         if x <= 0:
             x = -x
     if image_maple_rect.right >= w:
         if x > 0:
             x = -x
-    print(image_xian_rect.bottom, image_xian_rect.top)
     if pygame.Rect.colliderect(image_maple_rect, image_xian_rect):
-        if image_maple_rect.top + 1 == image_xian_rect.bottom and y < 0:
+        if image_maple_rect.top + 1 >= image_xian_rect.bottom and y < 0:
             y = -y
-        if image_maple_rect.bottom - 1 == image_xian_rect.top and y > 0:
+            pengzhuang.play()
+        if image_maple_rect.bottom - 1 <= image_xian_rect.top and y > 0:
+            num += 1
             y = -y
+            pengzhuang.play()
     # 更新画面
     pygame.display.update()
     # 速度设置
